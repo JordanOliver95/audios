f59e8b4f2a044ff2e38313da939abb669f1c1e12
Jordan Oliver <jordan.oliver@boldcommerce.com>
2019-07-12 13:54:27 -0500

    working api backend and start up scripts

---
+++ front-end/src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Container} from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="App">
+        <Container fixed maxWidth="md" className="App-container">
+            <div>hi</div>
+        </Container>
+    </div>
+  );
+}
+
+export default App;
